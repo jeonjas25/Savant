3d5f0a52317273aee0565c7e976250f706e260d8
Jason Jeong <jasonj0618@gmail.com>
2024-07-29 21:06:31 -0700

    TEst this blud

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
-from pybaseball import standings
-data = standings(2016)[4]
-print(data)
+from pybaseball import  statcast_pitcher
+kershaw_stats = statcast_pitcher('2017-06-01', '2017-07-01', 477132)
+kershaw_stats.groupby("pitch_type").release_speed.agg("mean")
